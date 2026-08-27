0f47f8cbc720cb8dfd2fefc2da776a4bcb50225a
Tom Marien <tommarien@gmail.com>
2026-08-27 09:19:39 +0200

    refactor: rebuild getOrDefault on top of get
    Reuse get() inside getOrDefault via errors.AsType to detect NotSetError,
    removing the duplicated LookupEnv/parse/ParseError logic. Also trims
    doc comments to be less repetitive.

--- envvar.go
+++ envvar.go
@@ -1,6 +1,7 @@
 package envvar
 
 import (
+	"errors"
 	"os"
 	"strconv"
 )
@@ -24,30 +25,22 @@ func get[T any](key, funcName string, parse ParseFunc[T]) (T, error) {
 }
 
 func getOrDefault[T any](key string, defaultValue T, funcName string, parse ParseFunc[T]) (T, error) {
-	envVar, ok := os.LookupEnv(key)
-	if !ok {
+	value, err := get(key, funcName, parse)
+	if _, ok := errors.AsType[NotSetError](err); ok {
 		return defaultValue, nil
 	}
 
-	value, err := parse(envVar)
-	if err != nil {
-		var zero T
-		return zero, ParseError{Func: funcName, Key: key, Value: envVar, Err: err}
-	}
-
-	return value, nil
+	return value, err
 }
 
-// String retrieves the value of the environment variable named by the key.
-// If the variable is present in the environment, the value (which may be empty) is returned.
-// If the variable is not present, it returns a [NotSetError].
+// String returns the value of the environment variable named by key.
+// If it is not set, it returns a [NotSetError].
 func String(key string) (string, error) {
 	return get(key, "String", func(v string) (string, error) { return v, nil })
 }
 
-// StringOrDefault retrieves the value of the environment variable named by the key.
-// If the variable is present in the environment, the value (which may be empty) is returned.
-// If the variable is not present, defaultValue is returned instead.
+// StringOrDefault returns the value of the environment variable named by key.
+// If it is not set, it returns defaultValue instead.
 func StringOrDefault(key, defaultValue string) string {
 	envVar, ok := os.LookupEnv(key)
 	if !ok {
@@ -56,18 +49,16 @@ func StringOrDefault(key, defaultValue string) string {
 	return envVar
 }
 
-// Int retrieves the value of the environment variable named by the key and converts it to an integer.
-// If the variable is present in the environment, the value is returned.
-// If the variable is not present, it returns a [NotSetError].
-// If the conversion fails it returns a [ParseError].
+// Int returns the value of the environment variable named by key, converted to an integer.
+// If it is not set, it returns a [NotSetError].
+// If the conversion fails, it returns a [ParseError].
 func Int(key string) (int, error) {
 	return get(key, "Int", strconv.Atoi)
 }
 
-// IntOrDefault retrieves the value of the environment variable named by the key and converts it to an integer.
-// If the variable is present in the environment, the value is returned.
-// If the variable is not present, defaultValue is returned instead.
-// If the conversion fails it returns a [ParseError].
+// IntOrDefault returns the value of the environment variable named by key, converted to an integer.
+// If it is not set, it returns defaultValue instead.
+// If the conversion fails, it returns a [ParseError].
 func IntOrDefault(key string, defaultValue int) (int, error) {
 	return getOrDefault(key, defaultValue, "IntOrDefault", strconv.Atoi)
 }
